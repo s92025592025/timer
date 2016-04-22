4c9a3f062e631f7f430aa03d32760c3d97b48be5
Flying_Apple_Pie <s92025592025@gmail.com>
2016-04-21 18:05:03 -0700

    final version

--- timer.js
+++ timer.js
@@ -3,6 +3,12 @@
 
 	var lastTime = [];
 	var timer = null;
+	var alarm = new Audio("alarm.mp3");
+	var totalTime;
+	var progressColor = ["#2ec521", "#2ec521", "#ef3714", "#8b2deb", "#1b2c33",
+						 "#27d4f4", "#cc86ed", "#f72a74", "#2d498e", "#3816f1",
+						 "#4f1d16", "#37904c", "#fca749", "#bb672e", "#eab4ac",
+						 "#e35510", "#dad212", "#d128b3", "#24e589", "#720111"];
 
 	window.onload = function(){
 		var addBtn = document.querySelectorAll(".add");
@@ -18,19 +24,23 @@
 		document.getElementById("Start").onclick = startTimer;
 		document.getElementById("Pause").onclick = pauseTimer;
 		document.getElementById("Restart").onclick = restartTimer;
+		document.getElementsByTagName("BODY")[0].onmousedown = stopAlarm;
+
+		document.getElementById("timer").style.backgroundColor = 
+			progressColor[Math.floor(Math.random() * progressColor.length)];
 	};
 
 	function addTime(){
 		var update = parseInt(this.parentElement.getElementsByTagName("SPAN")[0].innerHTML) + 1;
 		var digits = this.parentElement.parentElement.querySelectorAll(".digit");
 		this.parentElement.getElementsByTagName("SPAN")[0].innerHTML = Math.min(9, update);
-		correctTime();
+		totalTime = correctTime();
 	}
 
 	function decreaseTime(){
 		var update = parseInt(this.parentElement.getElementsByTagName("SPAN")[0].innerHTML) - 1;
 		this.parentElement.getElementsByTagName("SPAN")[0].innerHTML = Math.max(0, update);
-		correctTime();
+		totalTime = correctTime();
 	}
 
 	function startTimer(){
@@ -39,12 +49,11 @@
 			document.getElementById("Start").disabled = true;
 			document.getElementById("Pause").disabled = false;
 			document.getElementById("Restart").disabled = false;
-
 			lastTime = [];
 			for(var i = 0; i < digits.length; i++){
 				lastTime.push(digits[i].getElementsByTagName("SPAN")[0].innerHTML);
 			}
-
+			document.getElementById("timer").style.height = "0px";
 			timer = setInterval(countDown, 1000);
 		}
 	}
@@ -74,9 +83,19 @@
 
 	function countDown(){
 		document.querySelectorAll(".digit")[5].getElementsByTagName("SPAN")[0].innerHTML--;
+		document.getElementById("timer").style.height = ((totalTime - correctTime()) / totalTime * 100) + "%";
 		console.log(correctTime());
 		if(correctTime() == 0){
+			alarm.play();
 			pauseTimer();
+			document.getElementById("alert").style.display = "";
+			document.getElementsByTagName("BODY")[0].style.backgroundColor = 
+				document.getElementById("timer").style.backgroundColor;
+
+			document.getElementById("timer").style.height = "0px";
+
+			document.getElementById("timer").style.backgroundColor = 
+			progressColor[Math.floor(Math.random() * progressColor.length)];
 		}
 	}
 
@@ -86,9 +105,20 @@
 
 		document.getElementById("Start").disabled = false;
 		document.getElementById("Pause").disabled = true;
-		document.getElementById("Restart").disabled = true;
+		document.getElementById("Restart").disabled = false;
 	}
 
-	function restartTimer(){}
+	function restartTimer(){
+		var digits = document.querySelectorAll(".digit");
+		for(var i = 0 ; i < lastTime.length; i++){
+			digits[i].getElementsByTagName("SPAN")[0].innerHTML = lastTime[i];
+		}
+	}
+
+	function stopAlarm(){
+		alarm.pause();
+		alarm.load();
+		document.getElementById("alert").style.display = "none";
+	}
 
 })();
